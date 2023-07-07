d986d64d619ff7a8c094819e6ebab1efe958130d
Gennady Agashkov <g.agashkov@gmail.com>
2023-07-07 11:51:18 +0500

    Add seal and bolt states models

--- replaced_carts_report/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReplacedCartsReportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'replaced_carts_report'
--- replaced_carts_report/models.py
+++ replaced_carts_report/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class current_seal_state_for_front(models.Model):
+    # current_pallet_state = models.ForeignKey(
+    #     current_pallet_state,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True,
+    #     related_name="seal_state_for_front",
+    # )
+    # pallet = models.ForeignKey(
+    #     pallets,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True,
+    #     related_name="seal_states_for_front",
+    # )
+    time = models.DateTimeField(blank=True, null=True)
+    number = models.CharField(max_length=3, blank=True, null=True)
+ 
+    path_cam1_left = models.CharField(max_length=512, blank=True, null=True)
+    path_cam1_right = models.CharField(max_length=512, blank=True, null=True)
+    path_cam2_left = models.CharField(max_length=512, blank=True, null=True)
+    path_cam2_right = models.CharField(max_length=512, blank=True, null=True)
+ 
+    state_cam1_left = models.CharField(max_length=100, blank=True, null=True)
+    state_cam1_right = models.CharField(max_length=100, blank=True, null=True)
+    state_cam2_left = models.CharField(max_length=100, blank=True, null=True)
+    state_cam2_right = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "app_current_seal_state_for_front"
+
+
+class current_bolts_state_for_front(models.Model):
+    # current_pallet_state = models.ForeignKey(
+    #     current_pallet_state,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True,
+    #     related_name="bolts_state_for_front",
+    # )
+    # pallet = models.ForeignKey(
+    #     pallets,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True,
+    #     related_name="bolts_states_for_front",
+    # )
+    time = models.DateTimeField(blank=True, null=True)
+    number = models.CharField(max_length=3, blank=True, null=True)
+ 
+    path_cam1 = models.CharField(max_length=512, blank=True, null=True)
+    path_cam2 = models.CharField(max_length=512, blank=True, null=True)
+ 
+    state_cam1 = models.CharField(max_length=100, blank=True, null=True)
+    state_cam2 = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "app_current_bolts_state_for_front"
